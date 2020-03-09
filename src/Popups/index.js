1e5f219a00fd1eab6112a6360b4d628adc3aa9d5
Jaime Scowcroft <jaime.scowcroft@stockport.gov.uk>
2020-03-09 10:53:45 +0000

    change popup icons

--- src/Popups/index.js
+++ src/Popups/index.js
@@ -1,5 +1,5 @@
 const Land_Ownership_Popup = (feature, layer) => {
-  const content = `<div class="item"><i class="fa fa-tree" aria-hidden="true"></i><p class="title">Land Ownership</p>
+  const content = `<div class="item"><i class="far fa-image"></i><p class="title">Land Ownership</p>
     <p></p>
     <p class="info">Committee: ${feature.properties.committee_new}</p>
     <p class="info">Title Number: ${feature.properties.titlenumber}</p>
@@ -7,7 +7,7 @@ const Land_Ownership_Popup = (feature, layer) => {
   layer.bindPopup(content)
 }
 const Leases_Popup = (feature, layer) => {
-  const content = `<div class="item"><i class="fa fa-tree" aria-hidden="true"></i><p class="title">Leases</p>
+  const content = `<div class="item"><i class="fas fa-file-contract"></i><p class="title">Leases</p>
     <p></p>
     <p class="info">Description: ${feature.properties.description}</p>
     </div>`
